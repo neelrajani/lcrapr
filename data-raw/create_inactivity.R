080217c88dead19869e5eebfa52875af7cf36453
Neel Rajani <n_rajani@hotmail.com>
2018-03-20 16:58:42 +0000

    Turning raw data into usable .rda format

--- data-raw/create_inactivity.R
+++ data-raw/create_inactivity.R
@@ -3,7 +3,7 @@
 inactivitydata = read.csv("./raw_data/Inactivityanalysis.csv",check.names = TRUE)
 
 inactivitydata[, 2:3] <- lapply(inactivitydata[, 2:3], as.factor)
-inactivitydata[, 4:8] <- lapply(inactivitydata[, 4:8], as.number)
+inactivitydata[, 4:8] <- lapply(inactivitydata[, 4:8], as.numeric)
 colnames(inactivitydata)[5] = "VolunteerRate"
 
 devtools::use_data(inactivitydata)
